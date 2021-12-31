ac8a1e3b3dc60b3717ab249494e58362d04eb333
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2021-12-31 18:49:51 +1300

    regen fix and smaller thorny spriggan

--- 1.3/Source/ESCP_Spriggan/ESCP_Spriggan/Comps/Comp_SprigganRegeneration.cs
+++ 1.3/Source/ESCP_Spriggan/ESCP_Spriggan/Comps/Comp_SprigganRegeneration.cs
@@ -18,7 +18,7 @@ namespace ESCP_Spriggan
             base.PostPostApplyDamage(dinfo, totalDamageDealt);
 
             Pawn pawn = parent as Pawn;
-            if (!pawn.Dead && pawn.Spawned && pawn.health.summaryHealth.SummaryHealthPercent <= Props.minHealth && !pawn.health.hediffSet.HasHediff(Props.hediff))
+            if (!pawn.Dead && pawn.Spawned && pawn.health.hediffSet.PainTotal >= Props.minPain && !pawn.health.hediffSet.HasHediff(Props.hediff))
             {
                 pawn.health.AddHediff(Props.hediff).Severity = Props.Severity;
             }
